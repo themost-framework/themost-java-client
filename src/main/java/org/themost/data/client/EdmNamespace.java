04039050fc3c808d19965bec7052eddaff4b20d5
Kyriakos Barbounakis <k.barbounakis@gmai.com>
2021-04-29 11:22:55 +0300

    add edm terms

--- src/main/java/org/themost/data/client/EdmNamespace.java
+++ src/main/java/org/themost/data/client/EdmNamespace.java
@@ -4,5 +4,4 @@ package org.themost.data.client;
 public class EdmNamespace {
     static final String DefaultNamespace = "http://docs.oasis-open.org/odata/ns/edm";
     static final String EdmxNamespace = "http://docs.oasis-open.org/odata/ns/edmx";
-    static final String TermResourcePath = "Org.OData.Core.V1.ResourcePath";
 }
